3419e5941d81dcc3e4d73cd0a3a3241d859965bd
temakrivolapov <138175570+medowic@users.noreply.github.com>
2024-04-07 17:45:41 +0300

    added config support for cleaning time

--- clmem.sh
+++ clmem.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# This is memory cleaner script.
-# It already include in simple-memory-cleaner/install.sh
+if ! [ -f /etc/clmem.conf ]; then
+    echo "Couldn't find /etc/clmem.conf. Daemon will be stopped.";
+    exit 1;
+fi
+
+# shellcheck disable=SC1091
+source /etc/clmem.conf
+
+if [ -z "${CLEAN_TIME}" ]; then
+    echo "Couldn't resolve CLEAN_TIME in /etc/clmem.conf. Daemon will be stopped.";
+    exit 1;
+fi
 
 while true; do
-    sync; echo 1 > /proc/sys/vm/drop_caches
-    sleep 30m
+    sync;
+    echo 1 > /proc/sys/vm/drop_caches;
+    sleep "${CLEAN_TIME}"m;
 done

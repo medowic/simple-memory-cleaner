05925423d3eae8424f900e5603b7c0a9dc115ebb
temakrivolapov <138175570+medowic@users.noreply.github.com>
2025-01-13 21:46:02 +0300

    code refactoring

--- clmem.sh
+++ clmem.sh
@@ -1,20 +1,43 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-if ! [ -f /etc/clmem.conf ]; then
-    echo "Couldn't find /etc/clmem.conf. Daemon will be stopped.";
+if [ "${EUID}" -ne 0 ]; then
+    echo "Error: Script can start only by root user";
     exit 1;
 fi
 
-# shellcheck disable=SC1091
-source /etc/clmem.conf
-
-if [ -z "${CLEAN_TIME}" ]; then
-    echo "Couldn't resolve CLEAN_TIME in /etc/clmem.conf. Daemon will be stopped.";
-    exit 1;
-fi
-
-while true; do
+function cleanMemory() {
     sync;
     echo 1 > /proc/sys/vm/drop_caches;
-    sleep "${CLEAN_TIME}"m;
-done
+}
+
+if [ "${1}" == "--systemd" ]; then
+    if ! [ -f /etc/clmem.conf ]; then
+        echo "Warning: Couldn't find '/etc/clmem.conf'. Daemon will be cleaning memory every 30 minutes";
+        echo "Note: In /etc/clmem.conf file was added 'CLEAN_TIME' setting";
+        echo "CLEAN_TIME=30" | tee /etc/clmem.conf > /dev/null;
+        CLEAN_TIME=30;
+    else
+        # shellcheck disable=SC1091
+        source /etc/clmem.conf
+        if [ -z "${CLEAN_TIME}" ]; then
+            echo "Warning: Couldn't resolve values from '/etc/clmem.conf'. Daemon will be cleaning memory every 30 minutes";
+            echo "Note: In /etc/clmem.conf file was added 'CLEAN_TIME' setting";
+            echo "CLEAN_TIME=30" | tee /etc/clmem.conf > /dev/null;
+            CLEAN_TIME=30;
+        elif [ "${CLEAN_TIME}" -lt 1 ]; then
+            echo "Error: 'CLEAN_TIME' value in '/etc/clmem' is less than 1, so daemon was stopped"
+            exit 1;
+        fi
+    fi
+
+    while true; do
+        cleanMemory;
+        sleep "${CLEAN_TIME}"m;
+    done
+else
+    MEM_BEFORE=$(free | sed "2q;d" | awk '{print $6}')
+    cleanMemory;
+    MEM_AFTER=$(free | sed "2q;d" | awk '{print $6}')
+    echo "Cleaned: $(((MEM_BEFORE - MEM_AFTER) / 1024)) MiB"
+    exit 0;
+fi
--- install.sh
+++ install.sh
@@ -1,23 +1,39 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 if [ "${EUID}" -ne 0 ]; then
-    echo "";
-    echo "Script can start only by root user";
-    echo "";
+    echo "Error: Script can start only by root user";
     exit 1;
 fi
 
-mkdir /root/.clmem
+function downloadError() {
+    echo "Error: couldn't download '${1}' file from GitHub";
+    echo "Possible solutions:";
+    echo " - check your Internet connection";
+    echo " - is there 'curl' installed on your machine";
+    echo " - have you permissions to /usr/bin/";
+    echo "And try again";
+    exit 1;
+}
 
-curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.sh -o /root/.clmem/clmem.sh >/dev/null 2>&1
-curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.conf -o /etc/clmem.conf >/dev/null 2>&1
-curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.service -o /etc/systemd/system/clmem.service >/dev/null 2>&1
+if ! curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.sh -o /usr/bin/clmem > /dev/null 2>&1; then
+    downloadError clmem.sh;
+else
+    chmod 755 /usr/bin/clmem;
+    chmod +x /usr/bin/clmem;
+fi
 
-systemctl daemon-reload >/dev/null 2>&1
-systemctl start clmem >/dev/null 2>&1 
-systemctl enable clmem >/dev/null 2>&1
+if ! curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.conf -o /etc/clmem.conf > /dev/null 2>&1; then
+    downloadError clmem.conf;
+fi
 
-echo ""
-echo "Simple-Memory-Cleaner setup was successful. Memory cleaner can be stopped by 'systemctl stop clmem'"
-echo ""
-exit 0
+if ! curl https://raw.githubusercontent.com/medowic/simple-memory-cleaner/master/clmem.service -o /etc/systemd/system/clmem.service > /dev/null 2>&1; then
+    downloadError clmem.service;
+fi
+
+systemctl daemon-reload > /dev/null 2>&1
+systemctl start clmem > /dev/null 2>&1 
+systemctl enable clmem > /dev/null 2>&1
+
+echo "Simple Memory Cleaner was installed"
+echo "You can use it by run 'clmem' and start/stop daemon by 'systemctl stop clmem'"
+exit 0;
